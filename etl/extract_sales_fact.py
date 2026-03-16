6083ccab306b2367c69791b97e230aebef421ad3
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-16 14:25:43 +0700

    commit

---
+++ etl/extract_sales_fact.py
@@ -0,0 +1,53 @@
+import sys
+from pathlib import Path
+
+ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[2]
+sys.path.append(str(ROOT_FOLDER_LOCATION))
+
+import pandas as pd
+
+from plugins.local import InternalLocalExtractor
+from plugins.gspread import InternalGoogleSheetExtractor
+
+def extract(
+    *,
+    backend: str,
+    direction: str,
+) -> pd.DataFrame:
+    """
+    Generic Extract Dispatcher
+    ---
+    Principles:
+        1. Validate backend
+        2. Select correct extractor plugin
+        3. Trigger fetch process
+    ---
+    Returns:
+        pandas.DataFrame
+    """
+
+    print(
+        "🔄 [EXTRACT] Triggering to extract "
+        f"{backend} source {direction}..."
+    )
+
+    if backend == "local":
+        extractor = InternalLocalExtractor(direction)
+
+    elif backend == "gspread":
+        extractor = InternalGoogleSheetExtractor(direction)
+
+    else:
+        raise ValueError(
+            "❌ [EXTRACT] Failed to trigger extraction due to unsupported backend "
+            f"{backend}."
+        )
+
+    df = extractor.fetch()
+
+    print(
+        "✅ [EXTRACT] Extraction completed "
+        f"{len(df):,} row(s) fetched."
+    )
+
+    return df
